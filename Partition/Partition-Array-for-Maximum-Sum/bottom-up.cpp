291f73d30528c1e3e68262047eb5d2914d3abae3
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-08-11 13:46:47 +0530

    Add lecture 54 of DP Series

--- Partition/Partition-Array-for-Maximum-Sum/bottom-up.cpp
+++ Partition/Partition-Array-for-Maximum-Sum/bottom-up.cpp
@@ -1,21 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dp(int index, int k, int n, vector<int> &arr, vector<int> &cache) {
-	if (index == n) return 0;
-
-	int &ans = cache[index], maxElement;
-	if (ans != -1) return ans;
-
-	ans = 0, maxElement = 0;
-	for (int i = index; i < min(index + k, n); i++) {
-		maxElement = max(maxElement, arr[i]);
-		ans = max(ans, dp(i + 1, k, n, arr, cache) + (i - index + 1) * maxElement);
-	}
-
-	return ans;
-}
-
 int32_t main() {
 
 	int n, k; cin >> n >> k;
